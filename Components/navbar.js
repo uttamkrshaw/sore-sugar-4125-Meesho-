574269998367d45356f1904b60d1f020795dcaed
Deepak1095 <ddhankhar999@gmail.com>
2022-11-09 12:01:59 +0530

    home page

---
+++ Components/navbar.js
@@ -0,0 +1,16 @@
+function navbar(){
+    return ` <div id="header_left">
+    <img src="./image/logo.png" alt="">
+   
+</div>
+ 
+<div id="header_right">
+   
+    <h3 id="searchBtn">SEARCH</h3>
+
+    <h3 id="loginID"> LOGIN</h3>
+    <h3>HELP</h3>
+</div>`
+}
+
+export {navbar}

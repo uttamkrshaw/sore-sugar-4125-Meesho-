07cf818ed825905467600e1cf6bc03dd0bb78a2e
sainRaghuveer <raghuveersain987@gmail.com>
2022-11-11 18:08:53 +0530

    cart completed almost

--- Scripts/search.js
+++ Scripts/search.js
@@ -2,4 +2,113 @@ import {navbar} from "../Components/navbar.js"
 
 let header=document.querySelector("#header")
 header.innerHTML=navbar()
-console.log(navbar());
+// console.log(navbar());
+
+
+async function searchTerm(){
+
+    let search=document.getElementById("mysearch").value;
+    try{
+        
+
+        let response= await fetch(`https://boiling-brook-19493.herokuapp.com/posts?q=${search}`);
+
+        let data=await response.json();
+        // console.log("data:",data)
+
+        // appendData(data);
+        filter(data);
+    }
+    catch(error){
+        console.log("error:",error)
+    }
+}
+
+//filter functionality-->
+
+// let filter_btn=document.getElementById("price_filter");
+
+// filter_btn.addEventListener("change",function(){
+//     priceFilter();
+// });
+
+
+
+function filter(data){
+    let search=document.getElementById("mysearch").value;
+
+    let dataArr=[];
+    data.forEach((el)=>{
+        if(el.category==search){
+            dataArr.push(el);
+        }
+    })
+
+    appendData(dataArr);
+}
+
+function appendData(data){
+    // console.log(data)
+    let products_div=document.getElementById("products");
+
+    products_div.innerHTML=null;
+
+    data.forEach((el,i)=>{
+        console.log(el)
+        let div=document.createElement("div");
+        div.className="card"
+
+        let img=document.createElement("img");
+        img.src=el.image;
+        img.className="image_s";
+        img.addEventListener("click",()=>{
+            addToCart(el,i);
+            window.location.href="cartproductinfo.html"
+        })
+
+        let p_name=document.createElement("p");
+        p_name.innerText= el.discount;
+        p_name.className="title";
+
+        let p_price=document.createElement("p");
+        p_price.innerText=el.price;
+        p_price.className="prc"
+
+        let myp_div=document.createElement("div");
+        myp_div.className="flex";
+        myp_div.append(p_name,p_price)
+        div.append(img,myp_div);
+
+        products_div.append(div);
+
+    })
+}
+
+let searchFun=document.getElementById("mysearch");
+
+searchFun.addEventListener("input",function(){
+    debouncing(searchTerm,1000);
+})
+
+let id;
+
+function debouncing(func,delay){
+
+    if(id){
+        clearTimeout(id)
+    }
+    id=setTimeout(function(){
+
+        func();
+    },delay)
+
+}
+
+function addToCart(el,i){
+
+    let cartData=[];
+    cartData.push(el);
+
+    localStorage.setItem("cart",JSON.stringify(cartData));
+
+}
---
+++ Scripts/visacard.js
@@ -0,0 +1,39 @@
+let btn=document.getElementById("lastbtn");
+
+btn.addEventListener("click",function(){
+    cardValidate()
+    //console.log("new")
+})
+
+
+
+function cardValidate(){
+    console.log("name")
+    let cardnumber=document.getElementById("cardnumber").value;
+    
+    if(cardnumber.length<16 || cardnumber.length>16){
+        alert("INVALID CARD")
+    }
+
+    let cardHolder=document.getElementById("holder").value;
+    let month=document.getElementById("month").value;
+    //console.log(month)
+
+    let cvv=document.getElementById("CVV").value;
+    if(cvv.length<3 || cvv.length>3){
+        alert("INVALID CVV")
+    }
+
+    let year=document.getElementById("year").value;
+
+    let details={
+        cardnumber,
+        cardHolder,
+        month,
+        cvv,
+        year
+    }
+
+    console.log(details);
+}
+

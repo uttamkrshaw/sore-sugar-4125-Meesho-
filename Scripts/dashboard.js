c277c991c7a560cde6133389e7500db06d3b1580
uttamkrshaw <uttamkr5599@gmail.com>
2022-11-13 16:59:03 +0530

    admin page

---
+++ Scripts/dashboard.js
@@ -0,0 +1,30 @@
+
+
+
+const send_data = async() => {
+    let new_product_data = document.getElementById('new_products');
+
+    let data = {
+        "name":new_product_data.product_price.value,
+        "price":new_product_data.product_price.value,
+        "gender":new_product_data.product_gender.value,
+        "description":new_product_data.product_description.value,
+        "category":new_product_data.product_category.value,
+        "image":new_product_data.product_image.value,
+        "discount":new_product_data.product_name.value,
+    }
+    //console.log(data);
+
+    let response = await fetch(`https://boiling-brook-19493.herokuapp.com/posts`,{
+        method:"POST",
+        body:JSON.stringify(data),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    });
+    let response_data = await response.json();
+    console.log('data got from server', response_data);
+}
+
+let send_btn = document.getElementById('send_data');
+send_btn.addEventListener('click',send_data);

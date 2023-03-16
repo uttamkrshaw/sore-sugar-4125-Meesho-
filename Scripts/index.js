8b207499545185430ca0b90ee606aaade3a8af3b
sainRaghuveer <raghuveersain987@gmail.com>
2023-03-16 22:56:08 +0530

    new update with render data

--- Scripts/index.js
+++ Scripts/index.js
@@ -4,10 +4,6 @@ let header=document.querySelector("#header")
 header.innerHTML=navbar()
 //console.log(navbar());
 
-
-
-
-
 import { dropdown  } from "../Components/dropdown.js";
 let dropdownbtn=document.querySelector("#threedot")
 dropdownbtn.innerHTML=dropdown()
